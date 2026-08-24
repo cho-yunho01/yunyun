0b9067a9edd833ae76cc726e594130df79d6a85e
cho-yunho01 <04whdbsgh30@naver.com>
2026-08-24 15:28:12 +0900

    feat: jwt 구현

---
+++ src/main/java/com/delivery/yunyun/config/security/CustomAutehnticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.delivery.yunyun.config.security;
+
+import com.delivery.yunyun.dto.response.EntryPointErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class CustomAutehnticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        EntryPointErrorResponse entryPointErrorResponse = new EntryPointErrorResponse();
+        entryPointErrorResponse.setMsg("인증에 실패했습니다.");
+        response.setStatus(401);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(objectMapper.writeValueAsString(entryPointErrorResponse));
+    }
+}
