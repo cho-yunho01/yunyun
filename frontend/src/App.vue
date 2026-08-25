856f9449f5d220a5e45f892d1e5c04528e8efc7e
cho-yunho01 <04whdbsgh30@naver.com>
2026-08-25 19:05:23 +0900

    feat: jwt 확인

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,9 @@
 
 <template>
   <nav>
-    <router-link to = "/api/customer/create">사용자 회원가입</router-link>
+    <router-link to = "/api/customer/create">사용자 회원가입</router-link><br />
+    <router-link to = "/api/customer/login">사용자 로그인</router-link><br />
+    <router-link to = "/api/customer/id">사용자 ID</router-link>
   </nav>
   <div>
     <router-view />
