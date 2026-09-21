33bbda7484c8529eb652426b291052a519625017
cho-yunho01 <04whdbsgh30@naver.com>
2026-09-21 23:45:22 +0900

    fix : 사용자 수정

--- frontend/src/views/customer/CustomerInfo.vue
+++ frontend/src/views/customer/CustomerInfo.vue
@@ -1,10 +1,13 @@
 <script setup>
-import {ref, onMounted} from 'vue'
+import {ref, onMounted, reactive} from 'vue'
 import api from '@/api/axios';
 import CustomerDetail from '@/components/CustomerDetail.vue';
+import UpdateCustomerDetail from '@/components/UpdateCustomerDetail.vue';
 
 
 const customer = ref(null);
+
+const update = ref(false);
 
 const responseAPI = async () =>{
     const url = "/customer/info"
@@ -12,12 +15,43 @@ const responseAPI = async () =>{
     customer.value = response.data;
 }
 
+const updateMode = () => {
+    update.value = true;
+}
+
+const updateRequest = async (data) => {
+    const url = "/customer/update"
+    const customerRequest = reactive({
+        name : null,
+        userId : null,
+        password : data.password,
+        balance : data.balance
+    })
+
+    await api.put(url, customerRequest);
+    
+}
+
+
 onMounted( () =>{
-    responseAPI()
+    responseAPI(),
+    update.value = false;
 })
 </script>
 
 <template>
     <h1>사용자 정보</h1>
-    <CustomerDetail :customer = "customer" />
+    <div v-if = "update">
+        <CustomerDetail :customer = "customer" />
+    </div>
+    <div v-else>
+        <UpdateCustomerDetail 
+        :customer = "customer"
+        @update="updateRequest" />
+    </div>
+    <div v-if ="!update">
+        <button @click="updateMode">
+            수정하기
+        </button>
+    </div>
 </template>
